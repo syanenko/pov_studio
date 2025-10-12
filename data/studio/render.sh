df4b07da609a430da9d4d15674c1cde6f558a108
Sergey Yanenko <syanenko65@gmail.com>
2025-10-12 05:48:09 +0300

    Create render.sh

---
+++ data/studio/render.sh
@@ -0,0 +1,4 @@
+# Studio render
+# time ../../../povray/bin/povray -i./studio.pov -omodel ../../../povray/etc/povray/3.8/povray_800x600.ini
+time ../../../povray/bin/povray -i./studio.pov -omodel ../../../povray/etc/povray/3.8/povray_1920x1080.ini
+
